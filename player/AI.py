0e798bbeba1be7bcd4ea66ba7ecb6f8eeb91fa56
Ola-renee <85627670+Ola-renee@users.noreply.github.com>
2023-11-03 00:21:23 -0400

    Update AI.py

--- player/AI.py
+++ player/AI.py
@@ -251,28 +251,29 @@ class AI:
 
 
     def calculateb(self,gametiles):
-        piece_values = {
-                'P': -100, 'N': -350, 'B': -350, 'R': -525,
-                'Q': -1000, 'K': -10000,
-                'p': 100, 'n': 350, 'b': 350, 'r': 525,
-                'q': 1000, 'k': 10000
-                }
-        value = 0
-        for y in range(8):
-            for x in range(8):
-                piece_str = gametiles[y][x].pieceonTile.tostring()
-                piece_value = piece_values.get(piece_str, 0)
-                value += piece_value
-                if piece_str in ['P', 'p']:
-                    if x in [2, 3, 4, 5]:
-                        value += 10 if piece_str == 'p' else -10
-                if piece_str in ['N', 'n']:
-                    distance_center = max(abs(3.5 - x), abs(3.5 - y))
-                    value -= (8 - distance_center) * (20 if piece_str == 'n' else -20)
-                if piece_str in ['B', 'b']:
-                    diagonal_dist = min(y, x, 7 - y, 7 - x)
-                    value += diagonal_dist * (10 if piece_str == 'b' else -10)
-        return value
+    piece_values = {
+            'P': -100, 'N': -320, 'B': -330, 'R': -500,
+            'Q': -900, 'K': -20000,
+            'p': 100, 'n': 320, 'b': 330, 'r': 500,
+            'q': 900, 'k': 20000
+            }
+    value = 0
+    for y in range(8):
+        for x in range(8):
+            piece_str = gametiles[y][x].pieceonTile.tostring()
+            piece_value = piece_values.get(piece_str, 0)
+            value += piece_value
+            if piece_str in ['K', 'k']:
+                if x in [0, 1, 6, 7] or y in [0, 1, 6, 7]:
+                    value -= 100 if piece_str == 'K' else 100
+                else:
+                    value += 50 if piece_str == 'K' else -50
+            if piece_str in ['Q', 'q', 'R', 'r', 'B', 'b', 'N', 'n']:
+                value += len(gametiles[y][x].pieceonTile.legalMoves()) * (10 if piece_str.islower() else -10)
+            if piece_str in ['P', 'p']:
+                if x in [2, 3, 4, 5]:
+                    value += 10 if piece_str == 'p' else -10
+    return value
 
     def move(self,gametiles,y,x,n,m):
         promotion=False
